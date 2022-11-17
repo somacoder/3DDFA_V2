c8e1175347919bcc7fbce3077a1c3a1f30d2df03
somacoder <gkbartleyjr@gmail.com>
2022-11-17 16:33:23 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 source ~/anaconda3/etc/profile.d/conda.sh
-conda deactivate 3DDFA_V2
+conda deactivate
 conda env remove -n 3DDFA_V2
 conda env create -f environment.yml
 conda activate 3DDFA_V2
